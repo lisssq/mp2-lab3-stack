f1da2fc982d4cab795f717c73af818cb7acd11a4
lisssq <lizynka05@yandex.ru>
2024-10-31 15:30:06 +0300

    пытаемся создать операторы и методы

--- stack/stack.h
+++ stack/stack.h
@@ -8,16 +8,52 @@ class TStack
 	int MaxSize, Num;
 
 public:
-	TStack operator=(const TStack<T>& s);
+
+	TStack(int _MaxSize = 10);			//конструктор инициализации
+	~TStack();							//деструктор
+	TStack(const TStack& s);			//конструктор копирования
+
+	int GetSize()
+	{
+		return MaxSize;
+	}
+	int GetStartIndex()
+	{
+		return Num;
+	}
+
+	TStack operator=(const TStack<T> s);			//оператор присваивания
+	bool operator==(const TStack<T> s) const;		//сравнение равно
+	bool operator!=(const TStack<T> s) const;		//сравнение не равно
+
+
+	T Pop();
+	void Push(T val);
+	bool Check(string str);
+	void Clear();
+	TStack& operator=(const TStack<T>& s);
+
+
+	T Top() const;
+	bool Empty() const;
+	bool Full() const;
+
+
+	friend istream& operator>>(istream& in, TStack& s)
+	{
+
+	}
+
+	friend ostream& operator<<(ostream& out, const TStack& s)
+	{
+
+	}
 
 
 
 
 
-
-
-
-	bool Check(string stz)
+	/*bool Check(string stz)
 	{
 		TStack <char> s;
 		bool res = true;
@@ -41,9 +77,48 @@ public:
 			return false;
 		}
 		return true;
-	}
+	}*/
 
 
 };
 
+
+template <class T>
+TStack <T>::TStack(int _MaxSize)
+{
+
+}
+
+
+template <class T>
+TStack<T>::~TStack()
+{
+
+}
+
+
+template <class T>
+TStack<T>::TStack(const TStack& s)
+{
+
+}
+
+template <class T>
+TStack<T>& TStack<T>::operator=(const TStack<T>& s)
+{
+
+}
+
+template <class T>
+bool TStack<T>::operator==(const TStack<T>& s) const
+{
+
+}
+
+
+template <class T>
+bool TStack<T>::operator!=(const TStack<T>& s) const
+{
+
+}
 
